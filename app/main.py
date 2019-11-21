66471a9701c92e02a390a165679e21edc50371b6
Steve Harris <doc@steveharris.me>
2019-11-21 13:46:42 +0000

    reorganised sequence of main.py for routes

--- app/main.py
+++ app/main.py
@@ -1,26 +1,21 @@
 from starlette.applications import Starlette
 from starlette.responses import HTMLResponse
-from starlette.websockets import WebSocket
-from starlette.templating import Jinja2Templates
-from jinja2 import Template
-
+from starlette.routing import Route, Mount
 from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+from starlette.websockets import WebSocket
+
+# TODO do you need this as awell as the starlette template lib?
+from jinja2 import Template
 
 templates = Jinja2Templates(directory='app/templates')
 
-app = Starlette()
-
-
+# @app.route("/")
 async def homepage(request):
     return templates.TemplateResponse('index.html', {'request': request})
 
-routes = [
-    Route('/', endpoint=homepage),
-    Mount('/static', StaticFiles(directory='/app/static'), name='static')
-]
 
-
-@app.websocket_route("/ws")
+# @app.websocket_route("/ws")
 async def websocket_endpoint(websocket):
     await websocket.accept()
     # process incoming messages
@@ -28,3 +23,11 @@ async def websocket_endpoint(websocket):
         mesg = await websocket.receive_text()
         await websocket.send_text(mesg.replace("Client", "Server"))
     await websocket.close()
+
+
+routes = [
+    Route('/', endpoint=homepage),
+    Mount('/static', StaticFiles(directory='/app/static'), name='static')
+]
+
+app = Starlette(routes=routes)
