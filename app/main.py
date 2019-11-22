cb258df9ae98c7c4aed2c33cdd07f7efce67de16
Steve Harris <doc@steveharris.me>
2019-11-22 08:59:05 +0000

    it's a problem with routing and websockets

--- app/main.py
+++ app/main.py
@@ -4,26 +4,25 @@ from starlette.websockets import WebSocket
 from starlette.templating import Jinja2Templates
 from starlette.routing import Route, Mount
 from starlette.staticfiles import StaticFiles
-# from jinja2 import Template
-# import uvicorn
 
 templates = Jinja2Templates(directory="/app/templates")
 
 
 # @app.route('/')
 async def homepage(request):
-    # return HTMLResponse(Template(template).render())
     return templates.TemplateResponse("index.html", {"request": request})
 
 routes = [
     Route("/", endpoint=homepage),
-    Mount("/static", StaticFiles(directory="/app/static"), name="static")
+    Mount("/static", StaticFiles(directory="/app/static"), name="static"),
+    # TODO see below: enabling this route means the app fails
     # Route('/ws', endpoint=websocket_endpoint)
 ]
 
 app = Starlette(routes=routes)
 
 
+# TODO decorating the route is OK; but including in the routes array produces a 403 error
 @app.websocket_route('/ws')
 async def websocket_endpoint(websocket):
     await websocket.accept()
@@ -32,7 +31,3 @@ async def websocket_endpoint(websocket):
         mesg = await websocket.receive_text()
         await websocket.send_text(mesg.replace("Client", "Server"))
     await websocket.close()
-
-
-# if __name__ == '__main__':
-#     uvicorn.run(app, host='0.0.0.0', port=8000)
